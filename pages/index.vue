615ccd0e07dc01768894bd1961b31d487795b98a
SUETAK <akasirotonbo08451@gmail.com>
2021-10-24 21:05:04 +0900

    url, header の入力ができる

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,64 @@
 <template>
   <div>
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-text-field label="requestURL"/>
+          <div class="d-flex justify-space-around pa-6">
+            <v-flex>
+              <v-expansion-panels value="expanded" class="px-2">
+                <v-expansion-panel>
+                  <v-expansion-panel-header class="d-flex justify-space-around">common-header</v-expansion-panel-header>
+                  <v-expansion-panel-content v-for="header in commonHeader" v-bind="header">
+                    <div class="d-flex justify-space-around">
+                      <v-text-field label="commonHeader-key" class="px-6"/>
+                      <v-text-field label="commonHeader-value" class="px-6"/>
+                    </div>
+                  </v-expansion-panel-content>
+                </v-expansion-panel>
+              </v-expansion-panels>
+            </v-flex>
+            <v-btn class="pa-6" @click="addHeader(commonHeader)">addHeader</v-btn>
+          </div>
+          <v-row class="d-flex justify-space-around pa-6">
+            <div class="d-flex justify-space-around pa-6">
+              <v-flex>
+                <v-expansion-panels class="px-2">
+                  <v-expansion-panel>
+                    <v-expansion-panel-header class="d-flex justify-space-around">left-header</v-expansion-panel-header>
+                    <v-expansion-panel-content v-for="header in leftHeader" v-bind="header">
+                      <div class="d-flex justify-space-around">
+                        <v-text-field label="leftHeader-key" class="px-6"/>
+                        <v-text-field label="leftHeader-value" class="px-6"/>
+                      </div>
+                    </v-expansion-panel-content>
+                  </v-expansion-panel>
+                </v-expansion-panels>
+              </v-flex>
+              <v-btn class="pa-6" @click="addHeader(leftHeader)">addHeader</v-btn>
+            </div>
+            <div class="d-flex justify-space-around pa-6">
+              <v-flex>
+                <v-expansion-panels class="px-2">
+                  <v-expansion-panel>
+                    <v-expansion-panel-header class="d-flex justify-space-around">right-header</v-expansion-panel-header>
+                    <v-expansion-panel-content v-for="header in rightHeader" v-bind="header">
+                      <div class="d-flex justify-space-around">
+                        <v-text-field label="rightHeader-key" class="px-6"/>
+                        <v-text-field label="rightHeader-value" class="px-6"/>
+                      </div>
+                    </v-expansion-panel-content>
+                  </v-expansion-panel>
+                </v-expansion-panels>
+              </v-flex>
+              <v-btn class="pa-6" @click="addHeader(rightHeader)">addHeader</v-btn>
+            </div>
+          </v-row>
+          <v-btn class="d-flex justify-center mb-6">submit</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+
     <MonacoEditor
       :diffEditor="true"
       :original="left"
@@ -19,10 +78,20 @@ export default {
   components: {
     MonacoEditor
   },
-
-
   data() {
     return {
+      commonHeader: [{
+        key: "",
+        value: ""
+      }],
+      leftHeader: [{
+        key: "",
+        value: ""
+      }],
+      rightHeader: [{
+        key: "",
+        value: ""
+      }],
       options: {
         enableSplitViewResizing: false,
         // Render the diff inline
@@ -59,7 +128,24 @@ new Vue({
 `
     }
   },
+  // send を押す
+  // リクエストが行われる
+  // await でレスポンスが返却されるまで待機
+  // リクエストが返却される
+  // レスポンスで値を変更
+  // right, left の値の変化をwatch, computed で監視
+
   methods: {
+
+    request(url, header) {
+
+    },
+    addHeader(header) {
+      header.push({
+        key: "",
+        value: ""
+      })
+    },
     setLang(lang) {
       monaco.editor.setModelLanguage(
         this.$refs.diffViewEditor.getModifiedEditor().getModel(),
